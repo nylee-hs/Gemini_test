b6a931f382d4ec869ac1e4c62bb112411ff4861c
nylee <nylee@hs.ac.kr>
2024-11-10 17:12:47 +0900

    minor revise

--- st_chatbot.py
+++ st_chatbot.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import streamlit as st
 
 
-st.title('Gemini-Bot')
+st.title('이조수 챗봇')
 api_key = st.secrets['google_api_key']
 
 # @st.cache_resouce
@@ -20,6 +20,9 @@ if 'chat_session' not in st.session_state:
 for content in st.session_state.chat_session.history:
     with st.chat_message('ai' if content.role=='model' else 'user'):
         st.markdown(content.parts[0].text)
+
+with st.chat_message("ai"):
+    st.write("안녕하세요. 저는 이조수 입니다. 무엇이든 물어보세요~")
 
 if prompt := st.chat_input("메시지를 입력하세요."):
     with st.chat_message("user"):
